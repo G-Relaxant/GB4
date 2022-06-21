00105047a086dc71151934f5b6f9f30c99f1a7e0
Danya <theincredibledanzel@yandex.ru>
2022-06-21 20:21:05 +0400

    add 1

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for(int i = 1; i <= a; i++)
+{
+    sum += i; // sum = sum + i
+}
+
+Console.WriteLine(sum);
