0fc6cd46777b4b668810442a1c06048ed6440574
Danya <theincredibledanzel@yandex.ru>
2022-06-22 15:26:27 +0400

    3_ok

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+for(int i = 1; i <= num; i++)
+{
+    product = product * i;
+}
+if(num >= 1)
+{
+    Console.Write("Произведение чисел от 1 до N: " + product);
+}
+else
+{
+    Console.Write("Вы ввели недопустимое число");
+}    
