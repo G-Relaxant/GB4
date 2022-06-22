e1a5bdae0e880f091c1c4c3689188f683cfb8e0f
Danya <theincredibledanzel@yandex.ru>
2022-06-22 16:22:02 +0400

    4_edit

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,18 +1,19 @@
-﻿int[] array = new int[8];
-int lenght = array.Length;
+﻿// 1 вариант
+/*int[] array = new int[8];
+int length = array.Length;
 int index = 0;
-while (index < lenght)
+while (index < length)
 {
     array[index] = new Random().Next(0, 2);
     index++;
 }
 index = 0;
 Console.Write("[");
-while (index < lenght)
+while (index < length)
 {
     Console.Write(array[index]);
     index++;
-    if(index < lenght)
+    if(index < length)
     {
         Console.Write(",");
     }
@@ -21,4 +22,16 @@ while (index < lenght)
         Console.Write("");
     }
 }
-Console.Write("]");
+Console.Write("]");*/
+
+
+
+
+// 2 variant (проще и менее красивый + не точно удовлетворяет условиям задачи)
+int[] array = new int[8];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write(array[i] + " ");
+}
